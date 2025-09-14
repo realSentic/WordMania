98cb3acdf61508f14dd318adc80947147978d2dd
realSentic <syiamfortunado@gmail.com>
2025-09-14 16:38:41 +0800

    Add files via upload

---
+++ app_pages/about.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.title("ABOUT WORDMANIA")
+st.header("Welcome to WordMania!")
+
+st.write("---")
+
+st.subheader("Introduction")
+st.write("Hello, I'm Syiam, a second-year computer science student who loves making projects about interesting things! I've always loved learning new words, whether it's from a book i'm reading or from the dictionary. That's why I built WordMania, to help people expand their vocabulary with a fun twist; characters explaining them to you!")
+
+st.write("---")
+
+st.subheader("What is WordMania?")
+st.write("WordMania isn't your ordinary dictionary app. It gives you definitions and examples like every other dictionary apps out there, but it also adds a variety of characters who will help explain the words to you! Learning new words becomes interactive, entertaining, and memorable.")
+
+st.write("---")
+
+st.subheader("Why I built WordMania")
+st.write("Learning can be fun, it shouldn't be boring. WordMania was initially supposed to be a short side project, but I realized how fun it was building this project, so expect a few updates in the future!")
+
+st.write("---")
+
+st.subheader("Thank you for trying WordMania!")
+st.write("Explore new words, generate random words, see their meanings, and let characters help you understand them in the most entertaining way!")
+
+st.markdown(
+    """
+    <hr>
+    <p style='text-align: center; font-size:14px;'>
+    Made with ❤️ by <strong>Syiam Fortunado</strong> | 
+    <a href='https://facebook.com/senticreal/' target='_blank'>Facebook</a> | 
+    <a href='https://github.com/realSentic/' target='_blank'>GitHub</a> | 
+    <a href='https://www.instagram.com/sentic.s/' target='_blank'>Instagram</a>
+    </p>
+    """,
+    unsafe_allow_html=True
+)
---
+++ app_pages/main.py
@@ -0,0 +1,63 @@
+from google import genai
+import requests
+import streamlit as st
+
+# Dictionary
+def get_dictionary_word(word):
+    dict_url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+    dict_response = requests.get(dict_url)
+    if dict_response.status_code == 200:
+        dict_data = dict_response.json()
+        dict_result = []
+
+        for meaning in dict_data[0]["meanings"]:
+            part_of_speech = meaning.get("partOfSpeech", "N/A")
+            
+            for definitions in meaning["definitions"]:
+                definition_text = definitions.get("definition")
+                example_text = definitions.get("example", None)
+                
+                dict_result.append({
+                    "part_of_speech": part_of_speech,
+                    "definition_text": definition_text,
+                    "example_text": example_text
+                    })
+        return dict_result
+    else:
+        return None
+
+
+# AI
+client = genai.Client(api_key="AIzaSyAM9Vwf_KIO1BAJzX1Kh14bCJtlhvAa9wE")
+
+# Streamlit UI
+st.title("WordMania!")
+st.write("### Enter a word above to get started!")
+word = st.text_input("Search for a word")
+
+st.write("---")
+selection = st.selectbox("Explain like:", ["Pirate", "Shakespeare", "Naruto", "Wizard", "Goku", "Sherlock Holmes", "Vampire", "Napoleon Bonaparte", "Socrates", "Luffy", "Tsundere Girl", "Over-the-top battle announcer"])
+
+if not word.strip():
+    st.error("Please enter a word.")
+else:
+    with st.spinner("Please wait..."):
+        ai_response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents = f"You are {selection}, speaking exactly like {selection}'s character or stereotype. Explain the word '{word}' in exactly three sentences, no more, no less. Use {selection}'s typical tone, vocabulary, and style. Make it entertaining and clear for someone learning the word."   
+        )
+    st.write(ai_response.text)
+    st.write("---")
+
+if word:
+    results = get_dictionary_word(word)
+    if results:
+        for item in results:
+            st.write(f"***Part of Speech***: {item['part_of_speech'].capitalize()}")
+            st.write(f"***Definition***: {item['definition_text'].capitalize()}")
+            if item['example_text']:
+                st.write(f"***Example***: {item['example_text'].capitalize()}")
+            st.write("---")
+
+        
+          
